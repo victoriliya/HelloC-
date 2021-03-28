cb5ea94db43397b3bb6fc6171315d0e155518dba
victoriliya <victorbiliya@gmail.com>
2021-03-28 21:21:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HelloWorld{
+
+    public class{
+        public static void Main(string[] args){
+            System.Console.WriteLine("Welcome to another world");
+        }
+    }
+}
